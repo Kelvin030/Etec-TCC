ce93af31d689f7e27c1ec53aec87a16a1eb5e330
Kelvin <silva.com.br123007@gmail.com>
2024-10-16 22:06:17 -0300

    documentado

--- Etec/Backend/conectaDB.php
+++ Etec/Backend/conectaDB.php
@@ -2,10 +2,12 @@
 
 class conectaDB {
     
-    private $hostDB = "localhost";
-    private $usuarioDB = "root";
-    private $senhaDB = "SQLadmin1234";
-    private $bancoDB = "etecvestibulinho";
+    //  excute o script SQL (../SQL/createDatabase.sql) que é esta no repositorio, caso já tenha excutado o mesmo, sugiro dar um drop na tabela e executar novamente o script SQL
+
+    private $hostDB = "localhost"; // Alterar com seu host
+    private $usuarioDB = "root"; // Alterar com seu Usuario do banco
+    private $senhaDB = "SQLadmin1234"; // Alterar com sua Senha do banco
+    private $bancoDB = "etecvestibulinho"; // Manter nome do banco
     
     public function conecta(){
         $conn = new mysqli($this->hostDB, $this->usuarioDB, $this->senhaDB, $this->bancoDB);
